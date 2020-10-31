6780e83731e685f64e1aa98e8a86412ddd352083
Nicola Toscan <nicolatoscan99@outlook.it>
2020-10-31 19:02:48 +0100

    Init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace smart_hub_console_controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
