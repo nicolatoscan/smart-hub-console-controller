2bbd5c91105e1dbaf61078e1f3044c2b3525a1ed
Nicola Toscan <nicolatoscan99@outlook.it>
2020-10-31 19:08:29 +0100

    Controll light via args

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace smart_hub_console_controller
 {
@@ -6,7 +7,45 @@ namespace smart_hub_console_controller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No url found");
+                Environment.Exit(0);
+                return;
+            }
+            var url = args[0];
+            if (url != null)
+            {
+                Console.WriteLine(url);
+                try
+                {
+                    var json = new TimedWebClient().DownloadString(url);
+                    Console.WriteLine("OK");
+                }
+                catch
+                {
+                    Console.WriteLine("TIMEOUT");
+                }
+            }
+            Environment.Exit(0);
+        }
+    }
+
+    public class TimedWebClient : WebClient
+    {
+        // Timeout in milliseconds, default = 600,000 msec
+        public int Timeout { get; set; }
+
+        public TimedWebClient()
+        {
+            this.Timeout = 10000;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var objWebRequest = base.GetWebRequest(address);
+            objWebRequest.Timeout = this.Timeout;
+            return objWebRequest;
         }
     }
 }
